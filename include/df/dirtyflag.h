7085664a6a75ef69b4fec45dd73ccb75eefc9dd0
autogalkin <galigorrom@gmail.com>
2023-03-11 23:19:40 +0400

    finalize a flag storage customization

--- include/df/dirtyflag.h
+++ include/df/dirtyflag.h
@@ -1,112 +1,78 @@
 #pragma once
 #include <cstdint>
-#include <iostream>
 #include <type_traits>
 
 
 
 namespace df {
 
-enum class state : std::int8_t { clean = 0, dirty = 1 };
+enum class state : std::int8_t { clean = false, dirty = true };
+
+template<typename T, typename Object>
+concept is_dirtyflag_storage = requires(T t, Object obj, state new_state)
+{
+    {std::is_constructible_v<T, state> };
+    { t.mark(obj)     };
+    { t.clear(obj)    };
+};
 
 namespace flag_type {
 
-struct default_storage {
+struct bool_flag {
+    constexpr bool_flag(state init_state = state::dirty): state_(init_state){}
     template <typename DirtyFlagValue>
     constexpr bool is_dirty(DirtyFlagValue&) const noexcept {
         return static_cast<bool>(state_);
     }
     template <typename DirtyFlagValue>
-    constexpr void set_state(DirtyFlagValue&, state new_state) noexcept{
-        state_ = new_state;
-    }
-    state state_ = state::dirty;
-};
-
-struct logging
-{
-    template <typename DirtyFlagValue>
-    constexpr bool is_dirty(DirtyFlagValue&) const noexcept {
-        return false;
+    constexpr void mark(DirtyFlagValue&) noexcept{
+        state_ = state::dirty;
     }
     template <typename DirtyFlagValue>
-    constexpr void set_state(DirtyFlagValue& v,  state new_state) noexcept{
-        if(new_state == state::dirty) log(v);
+    constexpr void clear(DirtyFlagValue&) noexcept{
+        state_ = state::clean;
     }
-    template <typename DirtyFlagValue>
-    void log(DirtyFlagValue& v)
-    {
-        std::cout << "logging " << v;
-    }
-
+private:
+    state state_;
 };
 
 } // namespace flagtype
 
+template<class T, class FlagStorage = flag_type::bool_flag>
+requires is_dirtyflag_storage<FlagStorage, T>
+struct dirtyflag  final : private FlagStorage  {
 
-#pragma region internal
-namespace _internal {
-
-template<class T, class LogFunc, class FlagStorage = flag_type::default_storage>
-struct _dirty_flag_base : _dirty_flag_base<T, void> {
-    
-    using Base = _dirty_flag_base<T, void>;
-    constexpr explicit _dirty_flag_base(T object, const LogFunc& data, state start_state=state::dirty)
-        : Base(object, start_state), logger_(data) {}
-protected:
-    LogFunc logger_;
-};
-
-template<class T, class FlagStorage>
-struct _dirty_flag_base<T, void, FlagStorage > {
-    
-    constexpr explicit _dirty_flag_base(T object, state start_state=state::dirty) 
-        : object_(object){}
-protected:
-    constexpr void logger_() const noexcept {} // empty stub
-    T object_;
-    mutable FlagStorage flag_;
-    
-};
-} // namespace _internal
-
-#pragma endregion internal
-
-template<class T, class FlagStorage = flag_type::default_storage, class Log = void>
-struct dirtyflag  final : private _internal::_dirty_flag_base<T, Log, FlagStorage> {
-    using Base                  = _internal::_dirty_flag_base<T, Log, FlagStorage >;
-    using Base::Base;
-private:
 public:
-    constexpr dirtyflag(const dirtyflag & rhs )           noexcept(noexcept(logger_())) 
-             : Base(rhs)           { mark(); };
+    constexpr dirtyflag(T&& object, state init_state=state::dirty)        noexcept
+             : FlagStorage(init_state), object_(std::forward<T>(object)) {}
 
-    constexpr dirtyflag &operator=(const dirtyflag & rhs ) noexcept(noexcept(logger_()))  
+    constexpr dirtyflag(const dirtyflag & rhs )            noexcept(noexcept(mark()))
+             : FlagStorage(rhs)           { mark(); };
+
+    constexpr dirtyflag &operator=(const dirtyflag & rhs ) noexcept(noexcept(mark()))  
              { if(rhs.object_ != this->object_) {
                   mark(); 
-                  return Base::operator=(rhs);
+                  return FlagStorage::operator=(rhs);
                   }};
-
-    [[nodiscard]] constexpr const T  get() const noexcept 
+    [[nodiscard]] constexpr const T  get() const  noexcept 
                 requires ( std::is_pointer_v<T>) {         return object_;}
-    [[nodiscard]] constexpr       T  pin()       noexcept 
+    [[nodiscard]] constexpr       T  pin()        noexcept(noexcept(mark()))  
                 requires ( std::is_pointer_v<T>) { mark(); return object_;}
-    [[nodiscard]] constexpr const T& get() const noexcept(noexcept(logger_())) 
+    [[nodiscard]] constexpr const T& get() const  noexcept
                 requires (!std::is_pointer_v<T>) {         return object_;}
-    [[nodiscard]] constexpr       T& pin()       noexcept(noexcept(logger_()))  
+    [[nodiscard]] constexpr       T& pin()        noexcept(noexcept(mark()))  
                 requires (!std::is_pointer_v<T>) { mark(); return object_;}
 
-    constexpr void mark () noexcept(noexcept(logger_()))   { logger_(); flag_.set_state(object_, state::dirty) ; } 
-    constexpr void clear() noexcept { flag_.set_state(object_, state::clean) ; }
-    [[nodiscard]] constexpr bool is_dirty() const noexcept { return flag_.is_dirty(object_); }
+    constexpr void mark ()                        noexcept(noexcept(FlagStorage::mark(object_)))
+                    { FlagStorage::mark(object_) ;  } 
+    constexpr void clear()                        noexcept(noexcept(FlagStorage::clear(object_)))
+                    { FlagStorage::clear(object_) ; }
+    [[nodiscard]] constexpr bool is_dirty() const noexcept(noexcept(FlagStorage::is_dirty(object_)))
+                requires requires(T& dirtyflag_object)
+                {  { FlagStorage::is_dirty(dirtyflag_object) } -> std::convertible_to<bool>;}
+                    { return FlagStorage::is_dirty(object_); }
 private:
-    using Base::object_;
-    using Base::logger_;
-    using Base::flag_;
-}
-#ifdef DF_PACK
-__attribute__((__packed__))
-#endif
-;
+    T object_;
+};
 
 } // namespace df
--- tests/testdirtyflag/main.cpp
+++ tests/testdirtyflag/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include "df/dirtyflag.h"
+
+struct logging
+{
+    constexpr logging(df::state init_state){}
+    template <typename DirtyFlagValue>
+    constexpr void mark(DirtyFlagValue& v) noexcept{
+        log(v);
+    }
+    template <typename DirtyFlagValue>
+    constexpr void clear(DirtyFlagValue& v) noexcept{
+    }
+    template <typename DirtyFlagObj>
+    void log(DirtyFlagObj& v)
+    {
+        std::cout << "logging example " << v << std::endl;
+    }
+
+};
+
 int main()
 {
     
-    df::dirtyflag<char, df::flag_type::logging> df{'f'};
-    df.mark();
-    df.clear();
-    std::cout << df.is_dirty() << sizeof(df);
-
+    df::dirtyflag<char, logging> df{'f'};
+    df::dirtyflag<char, df::flag_type::bool_flag> df2{'a'};
+    df.pin() = 'a';
+    bool b = df.get() == df.get();
+    //std::cout << b;
+    //std::cout << df.pin() << sizeof(df) << df2.is_dirty();
     return 0;
 }

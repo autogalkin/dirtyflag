045dd21f07881e1d2f245703956ee85a57f144f3
autogalkin <galigorrom@gmail.com>
2023-03-20 22:32:46 +0300

    update dirty flag storages and the example file

--- include/df/dirtyflag.h
+++ include/df/dirtyflag.h
@@ -1,78 +1,138 @@
 #pragma once
+#include <concepts>
 #include <cstdint>
 #include <type_traits>
-
-
+#include <cassert>
 
 namespace df {
 
 enum class state : std::int8_t { clean = false, dirty = true };
 
-template<typename T, typename Object>
-concept is_dirtyflag_storage = requires(T t, Object obj, state new_state)
-{
-    {std::is_constructible_v<T, state> };
-    { t.mark(obj)     };
-    { t.clear(obj)    };
-};
+namespace storages {
 
-namespace flag_type {
+struct base_storage{};
 
-struct bool_flag {
-    constexpr bool_flag(state init_state = state::dirty): state_(init_state){}
-    template <typename DirtyFlagValue>
-    constexpr bool is_dirty(DirtyFlagValue&) const noexcept {
+class bool_storage {
+    state state_;
+public:
+    constexpr bool_storage(state init_state = state::dirty): state_(init_state){}
+    constexpr bool is_dirty() const noexcept {
         return static_cast<bool>(state_);
     }
-    template <typename DirtyFlagValue>
-    constexpr void mark(DirtyFlagValue&) noexcept{
+    constexpr void mark ()          noexcept{
         state_ = state::dirty;
     }
-    template <typename DirtyFlagValue>
-    constexpr void clear(DirtyFlagValue&) noexcept{
+    constexpr void clear()          noexcept{
         state_ = state::clean;
-    }
-private:
-    state state_;
+    }  
 };
 
-} // namespace flagtype
+template<auto& LogFuncPtr>
+requires requires(){LogFuncPtr();}
+struct logging{
+    logging(df::state init_state){
+        if(init_state == df::state::dirty) LogFuncPtr();
+    }
+    void mark() noexcept{ LogFuncPtr();}
+    constexpr void clear() noexcept{}
+};
 
-template<class T, class FlagStorage = flag_type::bool_flag>
-requires is_dirtyflag_storage<FlagStorage, T>
-struct dirtyflag  final : private FlagStorage  {
 
-public:
-    constexpr dirtyflag(T&& object, state init_state=state::dirty)        noexcept
-             : FlagStorage(init_state), object_(std::forward<T>(object)) {}
+template<auto& StaticStorageRef>
+requires requires(size_t i){
+    { StaticStorageRef[i] } -> std::convertible_to<df::state>;
+}
+struct static_storage {
+    constexpr static_storage(df::state init_state, size_t index_to_store){
+    StaticStorageRef[index_to_store] = init_state;
+    }
+    constexpr void mark(size_t index) noexcept{
+        StaticStorageRef[index] = df::state::dirty;
+    }
+    constexpr void clear(size_t index) noexcept{
+        StaticStorageRef[index] = df::state::clean;
+    }
+    constexpr bool is_dirty(size_t    index) const noexcept {
+        return static_cast<bool>(StaticStorageRef[index]);
+    }
+};
 
-    constexpr dirtyflag(const dirtyflag & rhs )            noexcept(noexcept(mark()))
-             : FlagStorage(rhs)           { mark(); };
+template<typename DynamicStorage>
+requires requires(DynamicStorage& storage, size_t i){
+    { storage[i] } -> std::convertible_to<df::state>;
+}
+struct dynamic_storage {
 
-    constexpr dirtyflag &operator=(const dirtyflag & rhs ) noexcept(noexcept(mark()))  
-             { if(rhs.object_ != this->object_) {
-                  mark(); 
-                  return FlagStorage::operator=(rhs);
-                  }};
-    [[nodiscard]] constexpr const T  get() const  noexcept 
-                requires ( std::is_pointer_v<T>) {         return object_;}
-    [[nodiscard]] constexpr       T  pin()        noexcept(noexcept(mark()))  
-                requires ( std::is_pointer_v<T>) { mark(); return object_;}
-    [[nodiscard]] constexpr const T& get() const  noexcept
-                requires (!std::is_pointer_v<T>) {         return object_;}
-    [[nodiscard]] constexpr       T& pin()        noexcept(noexcept(mark()))  
-                requires (!std::is_pointer_v<T>) { mark(); return object_;}
+    constexpr dynamic_storage(df::state init_state, DynamicStorage& storage, size_t index_to_store){
+        storage[index_to_store] = init_state;
+    }
+    constexpr void mark(DynamicStorage& storage, size_t  index) noexcept{
+        storage[index]  = df::state::dirty;
+    }
+    constexpr void clear(DynamicStorage& storage, size_t  index) noexcept{
+        storage[index] = df::state::clean;
+    }
+    constexpr bool is_dirty(const DynamicStorage& storage, size_t index) const noexcept {
+        return static_cast<bool>(storage[index]);
+    }
+};
 
-    constexpr void mark ()                        noexcept(noexcept(FlagStorage::mark(object_)))
-                    { FlagStorage::mark(object_) ;  } 
-    constexpr void clear()                        noexcept(noexcept(FlagStorage::clear(object_)))
-                    { FlagStorage::clear(object_) ; }
-    [[nodiscard]] constexpr bool is_dirty() const noexcept(noexcept(FlagStorage::is_dirty(object_)))
-                requires requires(T& dirtyflag_object)
-                {  { FlagStorage::is_dirty(dirtyflag_object) } -> std::convertible_to<bool>;}
-                    { return FlagStorage::is_dirty(object_); }
-private:
+} // namespace storage
+
+
+
+template<class T, class FlagStorage = storages::bool_storage>
+struct dirtyflag  final : public FlagStorage  {
+
     T object_;
+    
+public:
+
+    template <typename... Args>
+    constexpr dirtyflag(T &&object, state init_state = state::clean,
+                        Args... args) noexcept
+        : FlagStorage(init_state, args...), object_(std::forward<T>(object)) {}
+
+    using FlagStorage::mark;
+    using FlagStorage::clear;
+
+    [[nodiscard]] constexpr
+        typename std::conditional<std::is_trivial_v<T>, T, const T &>::type
+        get() const noexcept {
+            return object_; }
+
+    using ptr_or_ref_to_obj = typename std::conditional<std::is_pointer_v<T>, T,  T& >::type;
+    template<typename ... Args>
+    
+    [[nodiscard]] constexpr       ptr_or_ref_to_obj pin(Args&... args ) noexcept 
+                requires requires ( Args ...args) {{ mark( args... ) }; }
+    {   
+        mark(args...);
+        assert(FlagStorage::is_dirty(args...) == true && "a 'mark' function of FlagStorage is invalid a flag != df::state::dirty"); 
+        return object_;
+    }
+
+    [[nodiscard]] constexpr       ptr_or_ref_to_obj pin()              noexcept
+                  requires requires (T obj) {{ mark( obj ) }; } || requires{{ mark( ) }; }
+    { 
+        if constexpr(requires(){mark(object_);}){
+            mark(object_);
+        }
+        else{
+            mark();       
+        }       
+#ifndef DEBUG
+        // check result
+        constexpr auto error_message = "a 'mark' function of FlagStorage is invalid a flag != df::state::dirty";
+        if constexpr(requires(){FlagStorage::is_dirty(object_);}){
+            assert( FlagStorage::is_dirty(object_) == true && error_message); 
+        }
+        else if constexpr (requires(){FlagStorage::is_dirty();})  {
+            assert( FlagStorage::is_dirty()        == true && error_message); 
+        }     
+#endif
+        return object_;
+    }              
 };
 
 } // namespace df
--- tests/testdirtyflag/main.cpp
+++ tests/testdirtyflag/main.cpp
@@ -1,31 +1,116 @@
+#include <atomic>
+#include <concepts>
+#include <ios>
 #include <iostream>
 #include "df/dirtyflag.h"
+#include <type_traits>
+#include <vector>
+#include <cassert>
 
-struct logging
-{
-    constexpr logging(df::state init_state){}
-    template <typename DirtyFlagValue>
-    constexpr void mark(DirtyFlagValue& v) noexcept{
-        log(v);
+struct test_function_specialization{
+   test_function_specialization(df::state init_state){
     }
-    template <typename DirtyFlagValue>
-    constexpr void clear(DirtyFlagValue& v) noexcept{
+    template<typename Object>
+    void mark(Object obj) noexcept{
+        std::cout << "Hello World" << obj << std::endl;
+   }
+    void mark() noexcept{ // will newer call if specialization for dirty flag object exists
+        std::cout << "newer calls " << std::endl;
     }
-    template <typename DirtyFlagObj>
-    void log(DirtyFlagObj& v)
-    {
-        std::cout << "logging example " << v << std::endl;
+    constexpr void clear() noexcept{
     }
-
+    constexpr bool is_dirty(size_t index) const noexcept { return false; }
 };
+
+
+
 
 int main()
 {
+    {
+        std::cout << "default bool flag storage: \n";
+        df::dirtyflag<char, df::storages::bool_storage> bool_flag{'b'}; 
+        std::cout << "size of a char dirty flag: " << sizeof(bool_flag) << '\n';
+        assert(bool_flag.is_dirty() == false);
+        std::cout << "start value: '" << bool_flag.get() << "' start dirty:" << std::boolalpha << bool_flag.is_dirty() << '\n';
+        const char& value = bool_flag.get(); // get const value
+        assert(bool_flag.is_dirty() == false);
+        bool_flag.pin() = 'f';              // get non const ref
+        assert(bool_flag.is_dirty() == true);
+        std::cout << "after pin() the value is: '" << bool_flag.get()  << "' is dirty?- " << std::boolalpha << bool_flag.is_dirty() << "\n\n";
+    }
+
+    {
+        std::cout << "logging: \n";
+        static auto log = []{
+            std::cout << "hello world from the logging storage!" << std::endl;
+        };
+        df::dirtyflag<char, df::storages::logging<log>> log_flag{'l'};
+        assert(sizeof(log_flag) == sizeof(char)); // logging must be without a virtual table
+        std::cout << "size " << sizeof(log_flag) << '\n';
+        std::cout << "pin(): ";
+        log_flag.pin() = 'd';
+        std::cout << "\n";
+    }
+
+    {
+        std::cout << "static storage: \n";
+        static std::vector<df::state> static_storage_{1}; // or boost::dynamic_bitset, std::bitset
+        size_t index_to_store = 0;
+        df::dirtyflag<char, df::storages::static_storage< static_storage_> > ststorage_flag{'a', df::state::clean, index_to_store};
+        assert(sizeof(ststorage_flag) == sizeof(char));
+        std::cout << "size " << sizeof(ststorage_flag) << '\n';
+        std::cout << "start value: '" << ststorage_flag.get() << "' start dirty:" << std::boolalpha << ststorage_flag.is_dirty(index_to_store) << '\n';
+        std::cout << "pin(): ";
+        ststorage_flag.pin(index_to_store) = 'q';
+        assert(ststorage_flag.is_dirty(index_to_store) == true);
+        std::cout << "after pin() the value is: '" << ststorage_flag.get()  << "' is dirty?- " << std::boolalpha << ststorage_flag.is_dirty(index_to_store) << "\n\n";
+    }
+    {
+        std::cout << "dynamic storage: \n";
+        std::vector<df::state> dynamic_storage_{1};
+        dynamic_storage_[0] = df::state::clean;
+        dynamic_storage_.push_back(df::state::clean);
+        size_t index_to_store = 0;
+        df::dirtyflag<char, df::storages::dynamic_storage<  decltype(dynamic_storage_)> > dyn_storage{'g', df::state::clean, dynamic_storage_, index_to_store};
+        assert(sizeof(dyn_storage) == sizeof(char));
+        std::cout << "size " << sizeof(dyn_storage) << '\n';
+        std::cout << "start value: '" << dyn_storage.get() << "' start dirty:" << std::boolalpha << dyn_storage.is_dirty(dynamic_storage_, index_to_store) << '\n';
+        std::cout << "pin(): ";
+        dyn_storage.pin(dynamic_storage_, index_to_store) = 't';
+        assert(dyn_storage.is_dirty(dynamic_storage_, index_to_store) == true);
+        std::cout << "after pin() the value is: '" << dyn_storage.get()  << "' is dirty?- " << std::boolalpha << dyn_storage.is_dirty(dynamic_storage_, index_to_store) << "\n\n";
+    }
+
+    { 
+        std::cout << "check copy constructors: \n";
+        df::dirtyflag<char> original{'b'};
+        // copy constructors not affect on the flag, because we not change an original value
+        std::cout << "original, is dirty? - " << original.is_dirty() << " value = " << original.get() << '\n';
+        auto copy = original;
+        std::cout << "copy, is dirty? - " << original.is_dirty() << " value = " << original.get() << '\n';
+        auto copy_2{original};  
+        std::cout << "copy_2, is dirty? - " << original.is_dirty() << " value = " << original.get() << '\n'; 
+
+    }
+
     
-    df::dirtyflag<char, logging> df{'f'};
-    df::dirtyflag<char, df::flag_type::bool_flag> df2{'a'};
-    df.pin() = 'a';
-    bool b = df.get() == df.get();
+    
+    
+   // std::cout << df3.is_dirty(0);
+   // df3.pin( 0) = 'f';
+   // df3.mark(0);
+    
+    //df4.pin() = 'g';
+   // df3.mark(0 );
+    //df3.pin (0 ) = 'f';
+    //df3.clear(0);
+    //df2.pin() = 'f';
+   // std::cout << df3.get() << df3.is_dirty(0);
+
+    //df3.mark();
+    //df.pin() = 'a';
+    //bool b = df.get() == df.get();
     //std::cout << b;
     //std::cout << df.pin() << sizeof(df) << df2.is_dirty();
     return 0;
